d4f8d462237923d515281cdcd35dc633ab629ecd
khoiuu123456 <sevenboy2722000@gmail.com>
2021-07-12 14:54:55 +0700

    Hoàn tất ứng dụng thống kê covid các nước

--- app/src/main/java/com/example/rssnews/CovidActivity.java
+++ app/src/main/java/com/example/rssnews/CovidActivity.java
@@ -2,6 +2,7 @@ package com.example.rssnews;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -30,21 +31,58 @@ public class CovidActivity extends AppCompatActivity {
     private TextView todayConfirm,todayRecovered,todayDeath,datetime;
     private PieChart pieChart;
     private List<CountryData> list;
+    String country = "Vietnam";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_covid);
         list = new ArrayList<>();
+        if (getIntent().getStringExtra("country") != null)
+        {
+            country = getIntent().getStringExtra("country");
+        }
         Anhxa();
+        TextView cname = findViewById(R.id.cname);
+        cname.setText(country);
+        cname.setOnClickListener(v ->
+                startActivity(new Intent(CovidActivity.this,CountryCovidActivity.class)));
+        getThongTin(country);
 
+    }
 
+    private void setText(String updated) {
+        DateFormat format = new SimpleDateFormat("MMM dd, yyyy");
+
+        long miliseconds = Long.parseLong(updated);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(miliseconds);
+        datetime.setText("Updated at "+format.format(calendar.getTime()));
+    }
+
+    private void Anhxa() {
+        totalConfirm = findViewById(R.id.totalConfirm);
+        totalActive = findViewById(R.id.totalActive);
+        totalRecovered = findViewById(R.id.totalRecovered);
+        totalDeath = findViewById(R.id.totalDeath);
+        totalTests = findViewById(R.id.totalTests);
+        todayConfirm = findViewById(R.id.todayConfirm);
+        todayRecovered = findViewById(R.id.todayRecovered);
+        todayDeath = findViewById(R.id.todayDeath);
+        pieChart = findViewById(R.id.pieChart);
+        datetime = findViewById(R.id.date);
+
+    }
+
+    private void getThongTin(String country)
+    {
         ApiUtilities.getApiInterface().getCountryData().enqueue(new Callback<List<CountryData>>() {
             @Override
             public void onResponse(Call<List<CountryData>> call, Response<List<CountryData>> response) {
                 list.addAll(response.body());
                 for (int i =0;i<list.size();i++)
                 {
-                    if (list.get(i).getCountry().equals("Vietnam"))
+                    if (list.get(i).getCountry().equals(country))
                     {
                         int confirm = Integer.parseInt(list.get(i).getCases());
                         int active = Integer.parseInt(list.get(i).getActive());
@@ -77,31 +115,5 @@ public class CovidActivity extends AppCompatActivity {
                 Toast.makeText(CovidActivity.this,"Error :"+ t.getMessage(),Toast.LENGTH_SHORT).show();
             }
         });
-
-
-    }
-
-    private void setText(String updated) {
-        DateFormat format = new SimpleDateFormat("MMM dd, yyyy");
-
-        long miliseconds = Long.parseLong(updated);
-
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTimeInMillis(miliseconds);
-        datetime.setText("Updated at "+format.format(calendar.getTime()));
-    }
-
-    private void Anhxa() {
-        totalConfirm = findViewById(R.id.totalConfirm);
-        totalActive = findViewById(R.id.totalActive);
-        totalRecovered = findViewById(R.id.totalRecovered);
-        totalDeath = findViewById(R.id.totalDeath);
-        totalTests = findViewById(R.id.totalTests);
-        todayConfirm = findViewById(R.id.todayConfirm);
-        todayRecovered = findViewById(R.id.todayRecovered);
-        todayDeath = findViewById(R.id.todayDeath);
-        pieChart = findViewById(R.id.pieChart);
-        datetime = findViewById(R.id.date);
-
     }
 }
